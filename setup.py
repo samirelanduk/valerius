be5ca3aa4fd77dd9e8ae61cf1e6362f5090b7338
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-20 11:42:08 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+ name="genomia",
+ version="0.1.0",
+ description="Genome analysis tools.",
+ url="https://github.com/samirelanduk/genomia",
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Bio-Informatics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+ ],
+ keywords="biology bioinformatics genomes sequence",
+ install_requires=["requests"]
+)
