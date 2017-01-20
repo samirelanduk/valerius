d8402e25a177ea29f2a941c3fd55f84d7bbfe678
Sam Ireland <sam.ireland.uk@gmail.com>
2017-01-20 11:54:30 +0000

    Add validation to Genome sequence

--- genomia/genome.py
+++ genomia/genome.py
@@ -1,4 +1,10 @@
 class Genome:
 
     def __init__(self, sequence):
+        if not isinstance(sequence, str):
+            raise TypeError("Sequence must be str, not '%s'" % str(sequence))
+        chars_used = set(sequence)
+        for char in chars_used:
+            if char not in ("A", "T", "G", "C"):
+                raise ValueError("%s is not a valid base" % char)
         self._sequence = sequence
--- tests/test_genomes.py
+++ tests/test_genomes.py
@@ -6,3 +6,18 @@ class GenomeCreationTests(TestCase):
     def test_can_create_genome(self):
         genome = Genome("GCATCGTATACAGCAGTACGT")
         self.assertEqual(genome._sequence, "GCATCGTATACAGCAGTACGT")
+
+
+    def test_need_sequence(self):
+        with self.assertRaises(TypeError):
+            Genome()
+
+
+    def test_sequence_must_be_str(self):
+        with self.assertRaises(TypeError):
+            Genome(1001)
+
+
+    def test_only_valid_bases_allowed(self):
+        with self.assertRaises(ValueError):
+            Genome("GCATCGTATACAGCAGTACGTP")
