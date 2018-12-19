c6ef246057910350bfcc59ec8f160562f3cb24ba
Sam Ireland <sam.ireland.uk@gmail.com>
2018-12-19 15:16:55 +0000

    Restructure tests

---
+++ tests/integration/test_multiple_sequences.py
@@ -0,0 +1,22 @@
+from unittest import TestCase
+import valerius
+
+class SequenceSetTests(TestCase):
+
+    def test_sequence_interaction_tests(self):
+        pass
+
+
+    def test_raw_sequence_sets(self):
+        pass
+
+
+    def test_sequence_sets_from_list(self):
+        pass
+
+
+
+class SequenceSetOpeningTests(TestCase):
+
+    def test_can_open_sequences_from_file(self):
+        pass
