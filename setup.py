75770d8100d4feca3318eafb8445a7f0cc7f908f
Sam Ireland <sam.ireland.uk@gmail.com>
2018-12-19 15:10:54 +0000

    Start 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="valerius",
- version="0.2.0",
+ version="0.3.0",
  description="Biological sequence analysis tools.",
  url="https://valerius.samireland.com",
  author="Sam Ireland",
